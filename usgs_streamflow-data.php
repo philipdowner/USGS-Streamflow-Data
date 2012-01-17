52cfc0f6a5098b9ea484f0e028c99490579cb59a
Philip Downer <philip@manifestbozeman.com>
2012-01-17 14:57:55 -0700

    Move Read Me info to separate file. Increment versioning.

--- usgs_streamflow-data.php
+++ usgs_streamflow-data.php
@@ -3,35 +3,11 @@
 Plugin Name: USGS River Conditions
 Plugin URI: http://manifestbozeman.com
 Description: Set of functions to pull realtime streamflow data from the USGS.
-Version: 1.0
+Version: 1.01
 Author: Philip Downer
 Author URI: http://philipdowner.com
 License: GPL2
 */
-
-
-/******* README ******************
-
-Documentation: http://waterservices.usgs.gov/rest/IV-Service.html
-Data Source: http://waterservices.usgs.gov/nwis/iv?format=json,1.1&stateCd=mt&parameterCd=00060,00065,00010&siteType=ST
-Locate USGS sites: http://wdr.water.usgs.gov/nwisgmap/index.html
-
-********* PLANNED DEVELOPMENT ************************************
-~ Add temperature conversion option for Celsius to Fahrenheit
-~ Add plugin options page to allow easier administration
-~ Addition of shortcode to report on a site number(s)
-~ Use Google JS Maps API instead of Static Maps
-~ Automatic removal/hide of sites not reporting any data.
-~ Improved documentation
-
-******** USAGE ********************
-See each function for a list of accepted parameters.
-
-$sites = usgs_fetch_sites(array('jefferson','madison'));
-$parameters = usgs_fetch_dataParameters();
-$riverData = usgs_fetch_riverData($sites,$parameters);
-
-**/
 	
 /**
 * An array of rivers with their corresponding USGS site numbers
